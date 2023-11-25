ffb729a80684213ab889fe9728db80850128be68
ariffnii <aripmanul01@gmail.com>
2023-11-25 18:52:52 +0700

    template

---
+++ resources/views/admin/data-pegawai.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app-new')
+@section('konten')
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">
+                <h3>
+                    Table Pegawai
+                </h3>
+            </div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/beranda', function () {
-    return view('app-new');
+    return view('index');
 });
-
+Route::get('admin/data-pegawai', function () {
+    return view('admin/data-pegawai');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
